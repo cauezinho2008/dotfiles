2169e39df06503a9935bc134999381bb89e33284
Cauê Coelho <cauecoelho24@gmail.com>
2026-06-19 18:45:40 -0300

    moved oh my posh configs to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,4 +9,4 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH /home/caue/.lmstudio/bin
 # End of LM Studio CLI section
-oh-my-posh init fish --config '/home/caue/.poshthemes/blueish.omp.json' | source
+oh-my-posh init fish --config '/home/caue/.config/fish/oh_my_posh/blueish.omp.json' | source
