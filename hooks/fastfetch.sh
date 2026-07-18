cbf070ab89c704de6eddeed69dbd08601182b8aa
Cauê Coelho <cauecoelho24@gmail.com>
2026-07-17 22:36:34 -0300

    Revert "fastfetch hook: download distro logo from /etc/os-release"
    This reverts commit f677e63cb4156159e32f71bfd2fe85d6f2d75ebf.

--- hooks/fastfetch.sh
+++ hooks/fastfetch.sh
@@ -1,25 +1,2 @@
-source /etc/os-release 2>/dev/null
-DISTRO_ID="${ID:-linux}"
-DISTRO_LOGO_DIR="$HOME/.config/fastfetch"
-LOGO_FILE="$DISTRO_LOGO_DIR/logo"
-
-mkdir -p "$DISTRO_LOGO_DIR"
-
-LOGO_URL="https://raw.githubusercontent.com/everestlinux/DistroLogos/main/logos/${DISTRO_ID}.svg"
-
-if command -v curl &>/dev/null; then
-    curl -sL --connect-timeout 5 "$LOGO_URL" -o "$LOGO_FILE" 2>/dev/null || true
-fi
-
-if [[ ! -f "$LOGO_FILE" ]]; then
-    for icon in "/usr/share/icons/hicolor/128x128/apps/${DISTRO_ID}.png" \
-                "/usr/share/icons/hicolor/scalable/apps/${DISTRO_ID}.svg" \
-                "/usr/share/icons/hicolor/symbolic/apps/${DISTRO_ID}-symbolic.svg"; do
-        if [[ -f "$icon" ]]; then
-            cp "$icon" "$LOGO_FILE"
-            break
-        fi
-    done
-fi
-
-sed "s|/home/[^/]*/.config/fastfetch/[^\"']*|$LOGO_FILE|g" -i "$DISTRO_LOGO_DIR/config.jsonc" 2>/dev/null || true
+sed -i "s|/home/.*/.config/fastfetch/logo.webp|$HOME/.config/fastfetch/logo.webp|g" \
+~/.config/fastfetch/config.jsonc
